8d21d6058970b7c91541b03ffe26a2bf2bfaf01d
palex00 <32203971+palex00@users.noreply.github.com>
2025-03-14 23:40:41 +0100

    Add files via upload

--- scripts/utils.lua
+++ scripts/utils.lua
@@ -83,7 +83,7 @@ function toggle_item_grid(code)
     else
         Tracker:AddLayouts("layouts/item_grids.json")
     end
-
+        
     if extra_key_items and split_card_keys and split_passes and split_teas then
         Tracker:AddLayouts("layouts/items_no_ck_pass_tea.json")
         Tracker:AddLayouts("layouts/extra_items_all.json")
@@ -134,12 +134,21 @@ function toggle_item_grid(code)
     end
 end
 
-function toggle_split_map(code)
+function toggle_maps(code)
     local map_split = has("split_map_on")
-    if map_split then
+    local kanto_only = has("kanto_only_on")
+    if map_split and not kanto_only then
         Tracker:AddLayouts("layouts/map_layout_split.json")
-    else
+        Tracker:AddLayouts("layouts/maps.json")
+    elseif map_split and kanto_only then
+        Tracker:AddLayouts("layouts/map_layout_split_no_sevii.json")
+        Tracker:AddLayouts("layouts/maps_no_sevii.json")
+    elseif not map_split and not kanto_only then
         Tracker:AddLayouts("layouts/map_layout.json")
+        Tracker:AddLayouts("layouts/maps.json")
+    elseif not map_split and kanto_only then
+        Tracker:AddLayouts("layouts/map_layout_no_sevii.json")
+        Tracker:AddLayouts("layouts/maps_no_sevii.json")
     end
 end
 
